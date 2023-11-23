7f15aead52d9004a68cfb45161919725d436480d
elca-hub <csubaru07@gmail.com>
2023-11-24 08:59:20 +0900

    javaによるAPI構築に変更

---
+++ backend/src/main/java/com/mokimaki/arput/ArputApplication.java
@@ -0,0 +1,13 @@
+package com.mokimaki.arput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArputApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArputApplication.class, args);
+	}
+
+}
