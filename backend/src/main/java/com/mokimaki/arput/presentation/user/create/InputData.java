2055bf63dfac106859b098baf9570f8bf37f8a4a
elca-hub <csubaru07@gmail.com>
2023-11-24 09:18:51 +0900

    ADD: ユーザの新規作成時に使用するDTOのうち、inputを追加

---
+++ backend/src/main/java/com/mokimaki/arput/presentation/user/create/InputData.java
@@ -0,0 +1,8 @@
+package com.mokimaki.arput.presentation.user.create;
+
+public class InputData {
+    public String name;
+    public String email;
+    public String password;
+    public String passwordConfirmation;
+}
