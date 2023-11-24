5865f21370ed4f1bcb9b281a8234786708700dcd
elca-hub <csubaru07@gmail.com>
2023-11-24 11:28:05 +0900

    ADD: userのinputdataに学校名を追加

--- backend/src/main/java/com/mokimaki/arput/presentation/user/create/InputData.java
+++ backend/src/main/java/com/mokimaki/arput/presentation/user/create/InputData.java
@@ -1,8 +1,9 @@
 package com.mokimaki.arput.presentation.user.create;
 
-public class InputData {
-    public String name;
-    public String email;
-    public String password;
-    public String passwordConfirmation;
-}
+public record InputData(
+        String name,
+        String mailAddress,
+        String password,
+        String passwordConfirmation,
+        String schoolName
+) { }
