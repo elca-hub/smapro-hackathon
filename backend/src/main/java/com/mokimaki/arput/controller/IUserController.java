3e3cfac28200dca647f111870db43d24968b52b6
elca-hub <csubaru07@gmail.com>
2023-11-24 09:18:22 +0900

    ADD: ユーザコントローラの追加

---
+++ backend/src/main/java/com/mokimaki/arput/controller/IUserController.java
@@ -0,0 +1,7 @@
+package com.mokimaki.arput.controller;
+
+import com.mokimaki.arput.presentation.user.create.InputData;
+
+public interface IUserController {
+    void createUser(InputData inputData);
+}
