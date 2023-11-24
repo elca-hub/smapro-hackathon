022a9f71b2a4f90169a172f096fbddd013c9b33e
elca-hub <csubaru07@gmail.com>
2023-11-24 10:53:05 +0900

    CHANGE: 非推奨の書き方から推奨の書き方へ変更

--- backend/src/main/java/com/mokimaki/arput/infrastructure/security/SecurityConfig.java
+++ backend/src/main/java/com/mokimaki/arput/infrastructure/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,8 +17,8 @@ public class SecurityConfig {
                 .anyRequest().permitAll()
         );
 
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.csrf().disable();
+        http.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
