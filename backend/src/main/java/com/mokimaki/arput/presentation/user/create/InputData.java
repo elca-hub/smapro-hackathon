8faa1ccced5b35d1ea129b796cb2ded7730ba2c6
elca-hub <csubaru07@gmail.com>
2023-11-26 04:15:43 +0900

    UPDATE: InputDataのバリデーション作成

--- backend/src/main/java/com/mokimaki/arput/presentation/user/create/InputData.java
+++ backend/src/main/java/com/mokimaki/arput/presentation/user/create/InputData.java
@@ -1,9 +1,21 @@
 package com.mokimaki.arput.presentation.user.create;
 
+import jakarta.validation.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+
 public record InputData(
+        @Length(min = 1, max = 20)
         String name,
+
+        @Email
         String mailAddress,
+
+        @Length(min = 8, max = 20)
         String password,
+
+        @Length(min = 8, max = 20)
         String passwordConfirmation,
+
+        @Length(min = 1, max = 30)
         String schoolName
 ) { }
