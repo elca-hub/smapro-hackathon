3c5227376aceb9c6c768e9582d02fea3718a236e
elca-hub <csubaru07@gmail.com>
2023-11-26 04:03:23 +0900

    CHANGE: 現在作成されているモデルの属性の非NULLを明示

--- backend/src/main/java/com/mokimaki/arput/domain/model/user/User.java
+++ backend/src/main/java/com/mokimaki/arput/domain/model/user/User.java
@@ -1,19 +1,22 @@
 package com.mokimaki.arput.domain.model.user;
 
 import lombok.Getter;
-import lombok.Setter;
-
-import java.util.UUID;
+import lombok.NonNull;
 
 @Getter
 public class User {
-    private UserId id;
-    private String mailAddress;
-    private String name;
-    private String password;
-    private String schoolName;
+    @NonNull
+    private final UserId id;
+    @NonNull
+    private final String mailAddress;
+    @NonNull
+    private final String name;
+    @NonNull
+    private final String password;
+    @NonNull
+    private final String schoolName;
 
-    public User(UserId id, String mailAddress, String name, String password, String schoolName) {
+    public User(@NonNull UserId id, @NonNull String mailAddress, @NonNull String name, @NonNull String password, @NonNull String schoolName) {
         this.id = id;
         this.mailAddress = mailAddress;
         this.name = name;
@@ -21,7 +24,7 @@ public class User {
         this.schoolName = schoolName;
     }
 
-    public User(String mailAddress, String name, String password, String schoolName) {
+    public User(@NonNull String mailAddress, @NonNull String name, @NonNull String password, @NonNull String schoolName) {
         this.id = new UserId();
         this.mailAddress = mailAddress;
         this.name = name;
