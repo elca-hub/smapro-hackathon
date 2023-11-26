e73f741e7969549079279befa48b9ad1f6c70ced
elca-hub <csubaru07@gmail.com>
2023-11-26 11:35:23 +0900

    ADD: メールアドレスが既に登録されている場合エラーを返す

--- backend/src/main/java/com/mokimaki/arput/domain/service/user/UserService.java
+++ backend/src/main/java/com/mokimaki/arput/domain/service/user/UserService.java
@@ -2,9 +2,11 @@ package com.mokimaki.arput.domain.service.user;
 
 import com.mokimaki.arput.domain.model.user.User;
 import com.mokimaki.arput.domain.repository.IUserRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@Component
 public class UserService {
     private final IUserRepository userRepository;
 
