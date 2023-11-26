748bbfec0dc318c21c6914bf99c80964b0e45b78
elca-hub <csubaru07@gmail.com>
2023-11-27 07:03:26 +0900

    CHANGE: コンストラクタインジェクションに変更

--- backend/src/main/java/com/mokimaki/arput/infrastructure/security/MyRequestHeaderAuthenticationFilter.java
+++ backend/src/main/java/com/mokimaki/arput/infrastructure/security/MyRequestHeaderAuthenticationFilter.java
@@ -3,23 +3,17 @@ package com.mokimaki.arput.infrastructure.security;
 import com.mokimaki.arput.domain.repository.IUserRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedCredentialsNotFoundException;
 import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter;
 import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 
 public class MyRequestHeaderAuthenticationFilter extends RequestHeaderAuthenticationFilter {
     public MyRequestHeaderAuthenticationFilter(AuthenticationManager authenticationManager, IUserRepository userRepository) {
-        try {
-            setPrincipalRequestHeader("Authorization");
-            setExceptionIfHeaderMissing(false);
-            setAuthenticationManager(authenticationManager);
-            setRequiresAuthenticationRequestMatcher(
-                    new RegexRequestMatcher("/user/.*", null)
-            );
-        } catch (PreAuthenticatedCredentialsNotFoundException e) { // ヘッダーにAuthorizationがない場合
-            return;
-        }
-
+        setPrincipalRequestHeader("Authorization");
+        setExceptionIfHeaderMissing(false); // ヘッダにauthorizationがなくても認証処理を続行する
+        setAuthenticationManager(authenticationManager);
+        setRequiresAuthenticationRequestMatcher(
+                new RegexRequestMatcher("/user/.*", null)
+        );
 
         this.setAuthenticationSuccessHandler((request, response, authentication) -> {
             if (request.getRequestURI().equals("/user/logout")) {
