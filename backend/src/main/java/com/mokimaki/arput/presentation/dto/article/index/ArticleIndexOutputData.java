a21ba416221e899f2f63cd81b48f120ffc45ca76
elca-hub <csubaru07@gmail.com>
2023-11-30 02:25:46 +0900

    ADD: 記事の一覧を取得する際に評価も取得するように追加

--- backend/src/main/java/com/mokimaki/arput/presentation/dto/article/index/ArticleIndexOutputData.java
+++ backend/src/main/java/com/mokimaki/arput/presentation/dto/article/index/ArticleIndexOutputData.java
@@ -1,7 +1,10 @@
 package com.mokimaki.arput.presentation.dto.article.index;
 
+import java.util.Map;
+
 public record ArticleIndexOutputData (
         String articleId,
         String title,
-        String content
+        String content,
+        Map<String, Long> evaluations
 ) { }
