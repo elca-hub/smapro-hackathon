edee10ff196e2dc12c2247f78d55f8d62576a024
YAMAKI-Kengo <c1318809@st.kanazawa-it.ac.jp>
2023-12-01 01:47:24 +0900

    api

--- frontend/app/signin/page.tsx
+++ frontend/app/signin/page.tsx
@@ -11,10 +11,10 @@ export default function SigninPage() {
     async function onSubmit(event: FormEvent<HTMLFormElement>) {
       event.preventDefault()
       const sendData = {
-        email: email,
+        mailAddress: email,
         password: password
       }
-      const response = await fetch('http://localhost:3000/api/v1/auth/sign_in', {
+      const response = await fetch('http://localhost:5050/user/login', {
         method: 'POST',
         body: JSON.stringify(sendData),
         headers: {
@@ -31,30 +31,32 @@ export default function SigninPage() {
     return (
         <>
             <section className="text-gray-600 body-font py-44">
-                <div className="container mx-auto flex flex-wrap items-center justify-center">
-                    <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col w-full mt-10 md:mt-0">
-                        <h2 className="text-gray-900 text-lg font-medium title-font mb-5">サインイン</h2>
-                        <div className="relative mb-4">
-                            <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">メールアドレス</span>
-                            <input type="email" name="email" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="ここにメールアドレスを入力してください" 
-                                id="email"
-                                value={email}
+              <form onSubmit={onSubmit}>
+                  <div className="container mx-auto flex flex-wrap items-center justify-center">
+                      <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col w-full mt-10 md:mt-0">
+                          <h2 className="text-gray-900 text-lg font-medium title-font mb-5">サインイン</h2>
+                          <div className="relative mb-4">
+                              <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">メールアドレス</span>
+                              <input type="email" name="email" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="ここにメールアドレスを入力してください" 
+                                  id="email"
+                                  value={email}
+                                  required
+                                  onChange={(event) => setEmail(event.target.value)}
+                              />
+                          </div>
+                          <div className="relative mb-4">
+                              <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">パスワード</span>
+                              <input type="text" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="ここにパスワードを入力してください" 
+                                id="password"
+                                value={password}
                                 required
-                                onChange={(event) => setEmail(event.target.value)}
-                            />
-                        </div>
-                        <div className="relative mb-4">
-                            <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">パスワード</span>
-                            <input type="text" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" placeholder="ここにパスワードを入力してください" 
-                              id="password"
-                              value={password}
-                              required
-                              onChange={(event) => setPassword(event.target.value)}
-                            />
-                        </div>
-                        <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">サインイン</button>
-                    </div>
-                </div>
+                                onChange={(event) => setPassword(event.target.value)}
+                              />
+                          </div>
+                          <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" type="submit">サインイン</button>
+                      </div>
+                  </div>
+                </form>
             </section>        
         </>
 
