44130ecb0b64f51c8c5aa21b9820441e7d3e58b7
elca-hub <csubaru07@gmail.com>
2023-12-02 00:39:05 +0900

    ADD: APIを叩くことで記事を投稿できるように追加

--- frontend/app/post/create/page.tsx
+++ frontend/app/post/create/page.tsx
@@ -16,15 +16,28 @@ export default function CreateArticlePage () {
       content
     }
 
+    const token = localStorage.getItem('token')
+
+    if (!token) {
+      router.push('/signin')
+      return
+    }
+
     const res = await fetch('http://localhost:5050/article/', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Authorization': token
       },
       body: JSON.stringify(sendData)
     })
 
     const data = await res.json()
+
+    if (data.status === 'SUCCESS' && res.ok) {
+      router.push('/article')
+      return
+    }
   }
 
   return (
@@ -62,6 +75,15 @@ export default function CreateArticlePage () {
               onChange={(e) => setContent(e.target.value)}
               required
             ></textarea>
+          </div>
+
+          <div className="text-center my-6">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              type="submit"
+            >
+              投稿する
+            </button>
           </div>
         </form>
       </div>
