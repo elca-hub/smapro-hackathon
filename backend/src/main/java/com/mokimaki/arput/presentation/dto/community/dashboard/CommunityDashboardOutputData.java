04a8453ded52ead0ae032f5c9cb8cf2e91319bd4
elca-hub <csubaru07@gmail.com>
2023-12-01 22:58:55 +0900

    UPDATE: ユーザが作成したコミュニティの一覧を表示する際、idを戻り値として追加

--- backend/src/main/java/com/mokimaki/arput/presentation/dto/community/dashboard/CommunityDashboardOutputData.java
+++ backend/src/main/java/com/mokimaki/arput/presentation/dto/community/dashboard/CommunityDashboardOutputData.java
@@ -1,11 +1,13 @@
 package com.mokimaki.arput.presentation.dto.community.dashboard;
 
 public class CommunityDashboardOutputData {
+    public String id;
     public String name;
     public String description;
     public String entryCode;
 
-    public CommunityDashboardOutputData(String name, String description, String entryCode) {
+    public CommunityDashboardOutputData(String id, String name, String description, String entryCode) {
+        this.id = id;
         this.name = name;
         this.description = description;
         this.entryCode = entryCode;
