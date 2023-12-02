654a88788426da65a74672579e73b3c023afcfb0
elca-hub <csubaru07@gmail.com>
2023-12-02 11:48:34 +0900

    UPDATE: コミュニティの一覧表示の際に、メンバー数を戻すように変更

--- backend/src/main/java/com/mokimaki/arput/presentation/dto/community/index/CommunityIndexOutputData.java
+++ backend/src/main/java/com/mokimaki/arput/presentation/dto/community/index/CommunityIndexOutputData.java
@@ -9,10 +9,12 @@ public class CommunityIndexOutputData {
     public String name;
     public String description;
     public String communityId;
+    public int memberCount;
 
-    public CommunityIndexOutputData(String communityId, String name, String description) {
+    public CommunityIndexOutputData(String communityId, String name, String description, int memberCount) {
         this.communityId = communityId;
         this.name = name;
         this.description = description;
+        this.memberCount = memberCount;
     }
 }
